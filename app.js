c80cff608cd9005cee5adb0cc64f4c9589a8de82
liuyongkang <liuyongkang@appgame.com>
2018-10-30 16:39:11 +0800

    完成页面登录、登出操作

--- app.js
+++ app.js
@@ -1,17 +1,66 @@
 let express = require("express"),
     session = require("express-session"),
     ejs = require("ejs"),
+    md5 = require("md5-node"),
+    bodyParser = require("body-parser"),
     app = express();
 
 // 静态资源托管
 app.use(express.static("public"));
 
+// 配置body-parser中间件
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// 配置session中间件
+app.use(session({
+    secret: "session_id",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 600 * 1000  // 保存一分钟
+    },
+    rolling: true
+}));
+
 // ejs模板
 app.set("view engine", "ejs");
+
+// 判断登录状态中间件
+app.use((req, res, next) => {
+    if (req.url == "/login" || req.url == "/doLogin") {
+        next();
+    } else {
+        if (req.session.userInfo && req.session.userInfo.username != "") {
+            app.locals["userInfo"] = req.session.userInfo;
+            next();
+        } else {
+            res.redirect("/login")
+        }
+    }
+});
 
 // 登录页面
 app.get("/login", (req, res) => {
     res.render("login")
+});
+
+// 登录操作
+app.post("/doLogin", (req, res) => {
+    let userInfo = {
+        username: req.body.username,
+        password: md5(req.body.password), // 这里要进行加密
+    }
+    req.session.userInfo = userInfo;
+    res.redirect("/");
+});
+
+// 退出登录操作
+app.get("/logOut", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) return res.send("服务器有点小问题了，请刷新试试看~");
+        res.redirect("/");
+    });
 });
 
 // 首页
